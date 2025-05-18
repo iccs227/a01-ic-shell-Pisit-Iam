6230d3a5d0fa3024bf144c997d14ce34b340db5f
Pisit-Iam <pisit.iaa@student.mahidol.edu>
2025-05-18 14:43:27 +0700

    Milestone 1

--- icsh.c
+++ icsh.c
@@ -1,17 +1,95 @@
 /* ICCS227: Project 1: icsh
- * Name:
- * StudentID:
+ * Name:Pisit Iampattanatham
+ * StudentID:6280318
  */
 
 #include "stdio.h"
-
+#include "string.h"
+#include "stdlib.h"
+#include "sys/types.h"
+#include "sys/wait.h"
+#include "unistd.h"
+#include "errno.h"
+#include "signal.h"
+#include "fcntl.h"
 #define MAX_CMD_BUFFER 255
+#define MAX_CMD_ARGS 64
+char buffer[MAX_CMD_BUFFER];
+char last_command[MAX_CMD_BUFFER] = "";
+int exit_command = 0;
+int pid;
+int exit_status = 0;
+int bg_job = 0;
+int job_id = 0;
 
-int main() {
-    char buffer[MAX_CMD_BUFFER];
-    while (1) {
-        printf("icsh $ ");
-        fgets(buffer, 255, stdin);
-        printf("you said: %s\n", buffer);
+void command(char* buffer) {
+    char **args = (char **)malloc(sizeof(char *) * MAX_CMD_BUFFER);
+    char *command = (char *)malloc(sizeof(strlen(buffer) + 1));
+    strcpy(command, buffer);
+    char *sp_word = strtok(command, " ");
+    int i = 0;
+
+    int count = 0;
+    int redirect = 0;
+
+    while (sp_word != NULL) {
+        args[i] = sp_word;
+        sp_word = strtok(NULL, " ");
+        i++;
     }
+    args[i] = NULL;
+
+    if (strlen(buffer) == 0) {
+        return;
+    }
+    else if (!strncmp(buffer, "echo", 4)) {
+        if (buffer[4] == '\0' || strcmp(buffer + 5, "") == 0) {
+            printf("No text after echo command\n");
+        } else {
+            printf("%s\n", buffer + 5);
+            strcpy(last_command, buffer);
+        }
+    }
+    else if (strlen(buffer) == 2 && strncmp(buffer, "!!", 2) == 0) {
+        if (strlen(last_command) > 0) {
+            printf("%s\n", last_command);
+            printf("%s\n", buffer + 5);
+        }
+        else {
+            printf("No last command, back to the prompt\n");
+        }
+    }
+    else if (!strncmp(buffer, "exit", 4)) {
+        if (strlen(buffer) > 5) {
+            exit_command = atoi(buffer + 5);
+
+            if (exit_command>=0 && exit_command<=255) {
+                printf("bye\n");
+                exit(exit_command);
+            }
+            else{
+                printf("Wrong exit command\n");
+            }
+        }
+        else {
+            printf("Missing exit command\n");
+        }
+    }
+    else {
+        printf("bad command\n);")
+    }
+    free(args);
+}
+int main(int argc, char *argv[]) {
+    if {
+        printf("Starting IC Shell\n");
+        while (1) {
+            printf("icsh $ ");
+            fgets(buffer, 255, stdin);
+
+            buffer[strcspn(buffer, "\n")] = 0;
+            command(buffer);
+        }
+    }
+    return 0;
 }
